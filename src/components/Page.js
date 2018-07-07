cf833e39c60f477871c368c0401aef99477ed8ea
K.A. Schuneman <alex_schuneman@outlook.com>
2018-07-07 20:38:49 +0100

    feat: Added Content component to the Page

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import Content from './Content';
 
 class Page extends Component {
   render() {
+    const listObj = [
+      {
+        obj: 1,
+      },
+      {
+        obj: 2,
+      },
+      {
+        obj: 3,
+      },
+      {
+        obj: 4,
+      },
+    ];
     return (
       <div className="page__wrapper" >
         <h1>Hello</h1>
+        {Content(listObj)}
       </div>
     );
   }
