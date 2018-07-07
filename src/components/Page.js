b4fbcdba720c71ec22e6b7d0cf72dd9aa3a9a391
K.A. Schuneman <alex_schuneman@outlook.com>
2018-07-07 20:05:27 +0100

    feat: set up sass & page component

---
+++ src/components/Page.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Page extends Component {
+  render() {
+    return (
+      <div className="page__wrapper" >
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+export default Page;
