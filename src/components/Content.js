c441579dcad29301e7e4a6cb69e58766ba062f86
K.A. Schuneman <alex_schuneman@outlook.com>
2018-07-07 20:25:50 +0100

    feat: Added a simple component to handle content

---
+++ src/components/Content.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Content = (listOfObjects) => {
+  return listOfObjects.map(item => {
+    return (
+      <div className="items__wrapper">
+          <div className="item__content">
+            <img src="https://picsum.photos/288/198/?random" alt="random_image"/>
+            <div className="item__paraGraphs">
+            <p>
+            Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. 
+            Donec gravida libero quis lorem facilisis porttitor. 
+            Aenean sodales nulla enim. Nulla et felis leo. 
+            Ut ullamcorper consectetur lacus ut dignissim. 
+            Sed non tristique odio.
+          </p>
+          <p>
+          Nullam et dolor a velit posuere dictum. 
+            Vivamus gravida libero ac lorem varius, eu tincidunt nibh condimentum. 
+            Morbi erat nibh, vehicula in condimentum et, finibus vel nisl. 
+            Pellentesque lacus sem, venenatis sit amet urna sed, gravida laoreet nisi. 
+            Integer dignissim, nisi at posuere commodo, massa mi suscipit justo, ut aliquam lacus diam dignissim leo. 
+            In hac habitasse platea dictumst.
+          </p>
+            </div>
+          </div>
+        </div>
+    );
+  })
+}
+
+export default Content;
