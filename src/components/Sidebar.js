e009de74bd45c45d614b675ca01d9888b161edcb
K.A. Schuneman <alex_schuneman@outlook.com>
2018-07-07 20:41:12 +0100

    feat: Added a simple sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Sidebar = () =>  {
+    return (
+      <div className="sidebar__wrapper" >
+        <div className="sidebar__content">
+          <h1>SIDEBAR EXAMPLE</h1>
+        </div>
+      </div>
+    );
+  }
+
+export default Sidebar;
